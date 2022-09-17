7adcb31b7570560a52b7bc7204573d0d3e962d97
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-09-16 23:37:20 -0700

    Removed quotes around Fainted status so it's less sketchy

--- models/pokemoninstance.js
+++ models/pokemoninstance.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const PokemonInstanceSchema = new Schema(
     {
         pokemon: {type: Schema.Types.ObjectId, ref: "Pokemon", required: true},
-        status: { type: String, required: true, enum: ['Available', 'Adopted', 'Undergoing Rehabilitation', '"Fainted"'], default: 'Undergoing Rehabilitation' },
+        status: { type: String, required: true, enum: ['Available', 'Adopted', 'Undergoing Rehabilitation', 'Fainted'], default: 'Undergoing Rehabilitation' },
         birth_date: { type: Date, default: null },
         date_received: { type: Date, default: Date.now }
     }
