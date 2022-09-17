be4aa347f9acec18d09303b8e0341245964622fa
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-09-16 23:36:38 -0700

    Number in stock, verified status properties

--- models/pokemon.js
+++ models/pokemon.js
@@ -5,12 +5,12 @@ const PokemonSchema = new Schema(
   {
     name: {type: String, required: true, maxLength: 100},
     price: {type: Number, required: true, min: 0},
-    numberInStock: {type: Number},
+    number_in_stock: {type: Number},
     image: {type: String},
     types: {type: [Schema.Types.ObjectId], ref: 'Type',  required: true},
     weight: {type: Number, min: 0},
     height: {type: Number, min: 0},
-    isVerified: {type: Boolean}
+    is_verified: {type: Boolean}
   }
 );
 
