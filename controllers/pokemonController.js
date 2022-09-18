5ad9532f31141eb7fc1301af51f84caf23d04b92
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-09-18 00:37:34 -0700

    Nature count on index page

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,6 +1,7 @@
 const Pokemon = require('../models/pokemon');
 const PokemonInstance = require('../models/pokemoninstance');
 const Type = require('../models/type');
+const Nature = require('../models/nature');
 
 exports.index = (req, res) => {
     Promise.all([
@@ -8,7 +9,8 @@ exports.index = (req, res) => {
         PokemonInstance.countDocuments(),
         Type.countDocuments(),
         PokemonInstance.countDocuments({status:'Available'}),
-        PokemonInstance.countDocuments({status:'Adopted'})
+        PokemonInstance.countDocuments({status:'Adopted'}),
+        Nature.countDocuments()
 
     ]).then(results => {
         let data = {
@@ -16,7 +18,8 @@ exports.index = (req, res) => {
             pokemonInstance_count: results[1],
             type_count: results[2],
             available_pokemonInstance_count: results[3],
-            adopted_pokemonInstance_count: results[4]
+            adopted_pokemonInstance_count: results[4],
+            nature_count: results[5]
         }
         res.render('index', { title: "Pokedoption Index", data: data });
     }).catch(err => {
