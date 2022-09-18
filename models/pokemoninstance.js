0e722435af2559aa099df4a856a4206e632b5480
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-09-18 00:28:30 -0700

    nature property and url virtual

--- models/pokemoninstance.js
+++ models/pokemoninstance.js
@@ -6,8 +6,15 @@ const PokemonInstanceSchema = new Schema(
         pokemon: {type: Schema.Types.ObjectId, ref: "Pokemon", required: true},
         status: { type: String, required: true, enum: ['Available', 'Adopted', 'Undergoing Rehabilitation', 'Fainted'], default: 'Undergoing Rehabilitation' },
         birth_date: { type: Date, default: null },
-        date_received: { type: Date, default: Date.now }
+        date_received: { type: Date, default: Date.now },
+        nature: {type: Schema.Types.ObjectId, ref: "Nature", default: null}
     }
 );
+
+PokemonInstanceSchema
+  .virtual('url')
+  .get(function() {
+    return '/catalog/pokemoninstance/' + this.id;
+  });
 
 module.exports = mongoose.model('PokemonInstance', PokemonInstanceSchema);
