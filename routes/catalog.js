7bc0cd56de7125de3ef99d85de4b968633075734
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-09-18 23:18:55 -0700

    Type controller

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const pokemon_controller = require('../controllers/pokemonController');
+const type_controller = require('../controllers/typeController');
 
 //pokemon controller stuff
 router.get('/', pokemon_controller.index);
@@ -9,5 +10,10 @@ router.get('/', pokemon_controller.index);
 router.get('/pokemon', pokemon_controller.pokemon_list);
 
 router.get('/pokemon/:id', pokemon_controller.pokemon_detail);
+
+//Type controller stuff
+router.get('/type', type_controller.type_list);
+
+router.get('/type/:id', type_controller.type_detail);
 
 module.exports = router;
