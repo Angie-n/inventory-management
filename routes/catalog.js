7dd34ecadbc5514e2f41bfed556631e58a11b38e
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-09-18 23:56:48 -0700

    pokemoninstance controllers

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,17 +3,19 @@ const router = express.Router();
 
 const pokemon_controller = require('../controllers/pokemonController');
 const type_controller = require('../controllers/typeController');
+const pokemoninstance_controller = require('../controllers/pokemoninstanceController');
 
 //pokemon controller stuff
 router.get('/', pokemon_controller.index);
-
 router.get('/pokemon', pokemon_controller.pokemon_list);
-
 router.get('/pokemon/:id', pokemon_controller.pokemon_detail);
 
-//Type controller stuff
+//type controller stuff
 router.get('/type', type_controller.type_list);
-
 router.get('/type/:id', type_controller.type_detail);
+
+//pokemoninstance controller stuff
+router.get('/pokemoninstance', pokemoninstance_controller.pokemonInstance_list);
+router.get('/pokemoninstance/:id', pokemoninstance_controller.pokemonInstance_detail);
 
 module.exports = router;
