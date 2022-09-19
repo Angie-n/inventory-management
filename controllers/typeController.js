9168012f5c5eb4baabd2ccd28e06657bae61a386
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-09-18 23:18:39 -0700

    list and detail

---
+++ controllers/typeController.js
@@ -0,0 +1,22 @@
+const Type = require('../models/type');
+const Pokemon = require('../models/pokemon');
+
+exports.type_list = (req, res) => {
+    Type.find({}, 'name url')
+        .exec((err, result) => {
+            if(err) return err;
+            res.render('list', {title: "Type List", list: result})
+        });
+}
+
+exports.type_detail = (req, res) => {
+    Type.findById(req.params.id)
+        .exec((err, typeResult) => {
+            if(err) return err;
+            Pokemon.find({types: typeResult})
+                .exec((err, pokemonResult) => {
+                    if(err) return err;
+                    res.render('type_detail', {type: typeResult, pokemon: pokemonResult})
+                })
+        })
+}
